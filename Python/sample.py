55711e7b3ad832314394fed839d4342f28d56220
1994newbie <pratyush19standard@gmail.com>
2019-04-07 10:31:24 +0530

    second

---
+++ Python/sample.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+def ADD(x,y):
+    return x+y
+
+
+def ADD_TesT():
+    assert ADD(3,4) == 9, "sum should be 7"
+
+if __name__=="__main__":
+    ADD_TesT()
+    print("Everthing Passed")
